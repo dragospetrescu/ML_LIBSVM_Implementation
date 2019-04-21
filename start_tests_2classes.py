6be8446e6e9a2a79e4f3fa93493e49896e0fbf1e
Dragos Petrescu <dragos.petrescu@stud.acs.upb.ro>
2019-04-21 10:01:51 +0300

    added a two class script

---
+++ start_tests_2classes.py
@@ -0,0 +1,50 @@
+import subprocess
+from concurrent.futures import ThreadPoolExecutor
+import time
+import sys
+
+def threaded_func(args):
+    parameter = args[0]
+    fil = args[1]
+
+    test_name = parameter.replace(" ", "")
+    dir_name = fil.split('.')[0]
+    log_path = 'results/skin/' + str(dir_name) + '/' + str(test_name)
+    command = "python3.5 " + fil + " " + parameter
+    command_parts = command.split(' ')
+    with open(log_path, "wb", 0) as out:
+    	subprocess.run(command_parts, stdout=out, stderr=out, check=True)
+
+
+executor = ThreadPoolExecutor(max_workers=3)
+
+parameters = [
+    [
+    '-t 0',
+    '-t 1',
+    '-t 1 -d 1',
+    '-t 1 -d 5',
+    '-t 1 -g 0.5',
+    '-t 1 -g 1',
+    '-t 1 -r 0.0001',
+    '-t 1 -r 0.1',
+    '-t 2 -g 0.0001',
+    '-t 2 -g 0.5',
+    '-t 2 -g 1',
+    '-t 3',
+    '-t 3 -g 0.5',
+    '-t 3 -g 1',
+    '-t 3 -r 0.0001',
+    '-t 3 -r 0.1'
+    ]
+]
+files = ['svm_2class.py']
+
+for j in range(0, len(files)):
+    my_file = files[j]
+    my_parameters = parameters[j]
+    for i in range(0, len(my_parameters)):
+        parameter = my_parameters[i]
+        # executor.submit(threaded_func, (parameter, my_file))
+        threaded_func((parameter, my_file))
+executor.shutdown(wait=True)
--- svm_2class.py
+++ svm_2class.py
@@ -1,21 +1,28 @@
 from concurrent.futures import ThreadPoolExecutor
 import numpy
-
+import random
+import sys
+import datetime
 from svmutil import *
 
 
 def run_svm(parameter, y_train, x_train, y_predict, x_predict, no_classes):
-    print('Parameters: ' + parameter)
-    print('Marime set test: ' + str(len(y_predict)))
+
 
     m = svm_train(y_train, x_train, parameter)
     p_label, p_acc, p_val = svm_predict(y_predict, x_predict, m)
-    print(p_acc)
+
+    print('Parameters: ' + parameter)
+    print('Marime set test: ' + str(len(y_predict)))
+    print('Success rate predicting: ' + str(p_acc[0]))
+    print('Error rate predicting: ' + str(p_acc[1]))
 
     print('Matrice de confuzie set date testare')
     create_confussion_matrix(p_label, y_predict, no_classes)
 
     p_label, p_acc, p_val = svm_predict(y_train, x_train, m)
+    print('Success rate predicting: ' + str(p_acc[0]))
+    print('Error rate predicting: ' + str(p_acc[1]))
     print('Matrice de confuzie set date antrenare')
     create_confussion_matrix(p_label, y_train, no_classes)
 
@@ -31,39 +38,13 @@ def create_confussion_matrix(predict, actual, no_classes):
         print()
 
 # Read data in LIBSVM format
-y_train, x_train = svm_read_problem('input/skin/skin_training2')
-y_predict, x_predict = svm_read_problem('input/skin/skin_predict2')
+y_train, x_train = svm_read_problem('input/skin/skin_training')
+y_predict, x_predict = svm_read_problem('input/skin/skin_predict')
 executor = ThreadPoolExecutor(max_workers=3)
 
-parameters = [
-    # '-t 0',
-    # '-t 1 -d 3',
-    # '-t 1 -d 2',
-    # '-t 1 -d 1',
-    # '-t 1 -d 5',
-    # '-t 1 -g 0.0001',
-    # '-t 1 -g 0.1',
-    # '-t 1 -g 0.5',
-    # '-t 1 -g 1',
-    # '-t 1 -r 0',
-    # '-t 1 -r 0.0001',
-    # '-t 1 -r -0.0001',
-    # '-t 1 -r 0.1',
-    # '-t 1 -r -0.1',
-    # '-t 2 -g 0.0001',
-    # '-t 2 -g 0.01',
-    # '-t 2 -g 0.5',
-    # '-t 2 -g 1',
-    # '-t 3 -g 0.0001',
-    # '-t 3 -g 0.01',
-    # '-t 3 -g 0.5',
-    # '-t 3 -g 1',
-    # '-t 3 -r 0',
-    # '-t 3 -r 0.0001',
-    # '-t 3 -r -0.0001',
-    # '-t 3 -r 0.1',
-    '-t 3 -r -0.1'
-]
-for parameter in parameters:
-    executor.submit(run_svm(parameter, y_train[:5000], x_train[:5000], y_predict[:1000], x_predict[:1000], 2))
-executor.shutdown(wait=True)
+parameter = ''
+for i in range(1, len(sys.argv)):
+    parameter = str(parameter) + str(sys.argv[i]) + ' '
+print('Date: ' + str(datetime.datetime.now()))
+
+run_svm(parameter, y_train, x_train, y_predict, x_predict, 2)
